6e427f8524d2d9cc06b8df7cbb607a430c12238a
西风逍遥游 <sun_xfancy@163.com>
2018-02-08 11:37:28 -0800

    add personal config

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -2,13 +2,21 @@ from conans.model.conan_file import ConanFile
 from conans import CMake
 import os
 
+############### CONFIGURE THESE VALUES ##################
+default_user = "sunxfancy"
+default_channel = "testing"
+#########################################################
+
+channel = os.getenv("CONAN_CHANNEL", default_channel)
+username = os.getenv("CONAN_USERNAME", default_user)
 
 class DefaultNameConan(ConanFile):
     name = "DefaultName"
     version = "0.1"
     settings = "os", "compiler", "arch", "build_type"
     generators = "cmake"
-    requires = "gtest/1.8.0@lasote/stable"
+    build_policy = "missing"
+    requires = "gtest/1.8.0@%s/%s" % (username, channel)
         
     def build(self):
         cmake = CMake(self)
